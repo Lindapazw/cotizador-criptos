a8ba7c070dd04231ee21090bf9a001a862181936
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-28 15:54:52 -0300

    add Resultado styled

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,17 +1,37 @@
 import styled from "@emotion/styled"
 
+const Contenedor = styled.div`
+    color:#FFFFFF;
+    font-family:'Lato', sans-serif;
+
+`
+
+const Precio = styled.p`
+ font-size:25px;
+    span{
+        font-weight:700;
+    }
+`
+
+const Texto = styled.p`
+ font-size:15px;
+    span{
+        font-weight:700;
+    }
+`
+
 const Resultado = ({resultado}) => {
 
     const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE} = resultado
 
     return (
-        <div>
-            <p>El precio es de: <span>{PRICE}</span></p>
-            <p>El precio más alto del dia: <span>{HIGHDAY}</span></p>
-            <p>El precio más bajo del día: <span>{LOWDAY}</span></p>
-            <p>Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span></p>
-            <p>Última actualización: <span>{LASTUPDATE}</span></p>
-        </div>
+        <Contenedor>
+            <Precio>El precio es de: <span>{PRICE}</span></Precio>
+            <Texto>El precio más alto del dia: <span>{HIGHDAY}</span></Texto>
+            <Texto>El precio más bajo del día: <span>{LOWDAY}</span></Texto>
+            <Texto>Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span></Texto>
+            <Texto>Última actualización: <span>{LASTUPDATE}</span></Texto>
+        </Contenedor>
     )
 }
 
