ec1ae9d7b488e07f2446e2ccc3e0534ebd9f0b06
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-28 11:50:29 -0300

    add moneda file in formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled"
 import useSelectMonedas from "../hooks/useSelectMonedas"
+import {monedas} from '../data/monedas'
 
 const InputSubmit = styled.input `
     background-color: #9497ff;
@@ -21,9 +22,7 @@ const InputSubmit = styled.input `
 
 const Formulario = () => {
 
-    const [SelectMonedas] = useSelectMonedas('Elige tu moneda')
-
-    SelectMonedas()
+    const [SelectMonedas] = useSelectMonedas('Elige tu moneda', monedas)
 
     return (
         <div>
