2361c8195abe611bc9998818bc5290deed6953dc
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-28 10:47:38 -0300

    add InputSubmit styled

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,27 @@
+import styled from "@emotion/styled"
+
+const InputSubmit = styled.input `
+    background-color: #9497ff;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #fff;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius: 5px;
+    transition: background-color .3s;
+
+    &:hover {
+        background-color: #7a7dfe;
+        cursor: pointer;
+    }
+`
 
 const Formulario = () => {
     return (
         <div>
-            <input type="submit" value='Cotizar' />
+            <InputSubmit type="submit" value='Cotizar' />
         </div>
     )
 }
