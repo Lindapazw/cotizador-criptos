4e40c5f3f728f3fa6fc58ec3c23139f8f5d6f719
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-28 15:44:19 -0300

    add info to API in resultado

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -2,12 +2,16 @@ import styled from "@emotion/styled"
 
 const Resultado = ({resultado}) => {
 
-    console.log(resultado);
-    
+    const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE} = resultado
+
     return (
-        <h1>
-            Desde app
-        </h1>
+        <div>
+            <p>El precio es de: <span>{PRICE}</span></p>
+            <p>El precio más alto del dia: <span>{HIGHDAY}</span></p>
+            <p>El precio más bajo del día: <span>{LOWDAY}</span></p>
+            <p>Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span></p>
+            <p>Última actualización: <span>{LASTUPDATE}</span></p>
+        </div>
     )
 }
 
